21846d6eeca7b020da2e4e37854585968608e483
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-09 12:34:23 +0530

    Update add.py

--- add.py
+++ add.py
@@ -40,3 +40,29 @@ def to_wav_16k_mono(input_path: str, out_dir: str) -> str:
         check=True
     )
     return str(out_path)
+
+121.2 Successfully built antlr4-python3-runtime docopt julius
+121.2 ERROR: Exception:
+121.2 Traceback (most recent call last):
+121.2   File "/usr/lib/python3/dist-packages/pip/_internal/cli/base_command.py", line 165, in exc_logging_wrapper
+121.2     status = run_func(*args)
+121.2   File "/usr/lib/python3/dist-packages/pip/_internal/cli/req_command.py", line 205, in wrapper
+121.2     return func(self, options, args)
+121.2   File "/usr/lib/python3/dist-packages/pip/_internal/commands/install.py", line 389, in run
+121.2     to_install = resolver.get_installation_order(requirement_set)
+121.2   File "/usr/lib/python3/dist-packages/pip/_internal/resolution/resolvelib/resolver.py", line 188, in get_installation_order
+121.2     weights = get_topological_weights(
+121.2   File "/usr/lib/python3/dist-packages/pip/_internal/resolution/resolvelib/resolver.py", line 276, in get_topological_weights
+121.2     assert len(weights) == expected_node_count
+121.2 AssertionError
+------
+Dockerfile:24
+--------------------
+  22 |
+  23 |     COPY requirements.txt .
+  24 | >>> RUN pip3 install --no-cache-dir -r requirements.txt
+  25 |
+  26 |     COPY . .
+--------------------
+ERROR: failed to build: failed to solve: process "/bin/sh -c pip3 install --no-cache-dir -r requirements.txt" did not complete successfully: exit code: 2
+(base) root@EC03-E01-AICOE1:/home/CORP/re_nikitav/asr_whisperx#
