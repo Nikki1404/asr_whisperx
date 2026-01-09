f14e5222469f501366723abf6dc76d442e91ceb9
Nikki1404 <81031507+Nikki1404@users.noreply.github.com>
2026-01-09 13:20:02 +0530

    Update add.py

--- add.py
+++ add.py
@@ -1,27 +1,68 @@
-import whisperx
-from app.config import settings
 
-class WhisperXASR:
-    def __init__(self):
-        self.model = None
 
-    def load(self):
-        if self.model is None:
-            # VAD is enabled here for Faster-Whisper
-            self.model = whisperx.load_model(
-                settings.model,
-                device=settings.device,
-                compute_type=settings.compute_type,
-                vad=settings.vad
-            )
 
-    def transcribe(self, audio_chunk):
-        self.load()
 
-        # Faster-Whisper API (WhisperX 3.x)
-        # NO beam_size, NO vad_filter
-        return self.model.transcribe(
-            audio_chunk,
-            batch_size=settings.batch_size,
-            language=settings.language
-        )
+Container image Copyright (c) 2016-2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+
+This container image and its contents are governed by the NVIDIA Deep Learning Container License.
+By pulling and using the container, you accept the terms and conditions of this license:
+https://developer.nvidia.com/ngc/nvidia-deep-learning-container-license
+
+A copy of this license is made available in this container at /NGC-DL-CONTAINER-LICENSE for your convenience.
+
+INFO:     Started server process [1]
+INFO:     Waiting for application startup.
+INFO:     Application startup complete.
+INFO:     Uvicorn running on http://0.0.0.0:8003 (Press CTRL+C to quit)
+INFO:     172.17.0.1:36552 - "POST /asr/upload_folder HTTP/1.1" 500 Internal Server Error
+ERROR:    Exception in ASGI application
+Traceback (most recent call last):
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/protocols/http/h11_impl.py", line 410, in run_asgi
+    result = await app(  # type: ignore[func-returns-value]
+  File "/usr/local/lib/python3.10/dist-packages/uvicorn/middleware/proxy_headers.py", line 60, in __call__
+    return await self.app(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/applications.py", line 1135, in __call__
+    await super().__call__(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/applications.py", line 107, in __call__
+    await self.middleware_stack(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/middleware/errors.py", line 186, in __call__
+    raise exc
+  File "/usr/local/lib/python3.10/dist-packages/starlette/middleware/errors.py", line 164, in __call__
+    await self.app(scope, receive, _send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/middleware/exceptions.py", line 63, in __call__
+    await wrap_app_handling_exceptions(self.app, conn)(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/_exception_handler.py", line 53, in wrapped_app
+    raise exc
+  File "/usr/local/lib/python3.10/dist-packages/starlette/_exception_handler.py", line 42, in wrapped_app
+    await app(scope, receive, sender)
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/middleware/asyncexitstack.py", line 18, in __call__
+    await self.app(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/routing.py", line 716, in __call__
+    await self.middleware_stack(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/routing.py", line 736, in app
+    await route.handle(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/routing.py", line 290, in handle
+    await self.app(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/routing.py", line 115, in app
+    await wrap_app_handling_exceptions(app, request)(scope, receive, send)
+  File "/usr/local/lib/python3.10/dist-packages/starlette/_exception_handler.py", line 53, in wrapped_app
+    raise exc
+  File "/usr/local/lib/python3.10/dist-packages/starlette/_exception_handler.py", line 42, in wrapped_app
+    await app(scope, receive, sender)
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/routing.py", line 101, in app
+    response = await f(request)
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/routing.py", line 355, in app
+    raw_response = await run_endpoint_function(
+  File "/usr/local/lib/python3.10/dist-packages/fastapi/routing.py", line 243, in run_endpoint_function
+    return await dependant.call(**values)
+  File "/app/app/main.py", line 36, in upload_folder
+    result, timings = pipeline.run(wav, min_speakers, max_speakers)
+  File "/app/services/pipeline.py", line 26, in run
+    res = self.asr.transcribe(chunk)
+  File "/app/services/whisperx_asr.py", line 19, in transcribe
+    self.load()
+  File "/app/services/whisperx_asr.py", line 11, in load
+    self.model = whisperx.load_model(
+  File "/usr/local/lib/python3.10/dist-packages/whisperx/__init__.py", line 21, in load_model
+    return asr.load_model(*args, **kwargs)
+TypeError: load_model() got an unexpected keyword argument 'vad'
